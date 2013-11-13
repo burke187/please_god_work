1ad14d73712ceb80592dac462fd96a075a6fba01
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-13 11:20:24 -0600

    removed stupid htmls

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,6 @@ NTouch::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.assets.compress = false
-  config.assets.debug = true
-
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
 end
